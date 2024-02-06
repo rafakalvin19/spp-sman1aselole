f266cee443a87812e4c7563030858f170d5b3aca
rafakalvin19 <kalvinrafa7@gmail.com>
2024-02-06 09:38:02 +0700

    Tambah Modal Lunasi

--- laporan.php
+++ laporan.php
@@ -71,6 +71,77 @@ if (empty($_SESSION['user']['level'])) {
                                 </td>
                             </tr>
 
+                              <!-- Modal Edit -->
+                              <div class="modal fade" id="lunasi<?php echo $data['id_pembayaran'] ?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <div class="col-12">
+                                                <div class="text-center">
+                                                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Lunasi</h1>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <form method="post" action="aksi-crud.php">
+                                            <div class="modal-body">
+                                                <div class="row">
+                                                    <div class="mb-4">
+                                                        <input type="hidden" name="id_pembayaran" value="<?= $data['id_pembayaran'] ?>">
+                                                        <label class="form-label">Nama Siswa</label>
+                                                        <select name="nisn" class="form-select" value="<?= $data['nisn'] ?>" disabled>
+                                                            <?php
+                                                            $query1 = mysqli_query($koneksi, "SELECT * FROM siswa");
+                                                            while ($siswa = mysqli_fetch_array($query1)) {
+                                                            ?>
+                                                                <option value="<?php echo $siswa['nisn'] ?>" <?php if ($data['nisn'] == $siswa['nisn']) {
+                                                                                                                    echo 'selected';
+                                                                                                                } ?>><?php echo $siswa['nama'] ?></option>
+                                                            <?php
+                                                            }
+                                                            ?>
+                                                        </select>
+                                                    </div>
+                                                    <input type="hidden" name="tanggal_bayar" class="form-control" value="<?php echo date('Y-m-d'); ?>">
+                                                    <div class="mb-4">
+                                                        <label class="form-label">SPP</label>
+                                                        <select name="id_spp" class="form-select" value="<?= $data['id_spp'] ?>" disabled>
+                                                            <?php
+                                                            $query2 = mysqli_query($koneksi, "SELECT * FROM spp");
+                                                            while ($spp = mysqli_fetch_array($query2)) {
+                                                            ?>
+                                                                <option value="<?php echo $spp['id_spp'] ?>" <?php if ($data['id_spp'] == $spp['id_spp']) {
+                                                                                                                    echo 'selected';
+                                                                                                                } ?>><?php echo $spp['tahun'] ?> - <?php echo $spp['nominal'] ?></option>
+                                                            <?php
+                                                            }
+                                                            ?>
+                                                        </select>
+                                                    </div>
+                                                    <div class="mb-4">
+                                                        <label class="form-label">Kekurangan</label>
+                                                        <input type="text" name="kekurangan" class="form-control" value="<?php echo $data['nominal'] - $data['jumlah_bayar'] ?>" readonly>
+                                                    </div>
+                                                    <div class="mb-4">
+                                                        <label class="form-label">Jumlah Bayar</label>
+                                                        <input type="text" name="jumlah_bayar" class="form-control" required>
+                                                        <input type="hidden" name="oldjumlah_bayar" class="form-control" value="<?php echo $data['jumlah_bayar'] ?>">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <div class="col-12">
+                                                    <div class="text-center">
+                                                        <button type="submit" class="btn btn-success" name="lunasi">Simpan</button>
+                                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Keluar</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--Akhir Modal Edit-->
+
                         <?php
                         }
                         ?>
